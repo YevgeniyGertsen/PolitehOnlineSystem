a9f99a5b1856fae92995a10570515c582f82d61f
Nurbolat Azhgaliev <nur9609@outlook.com>
2023-09-19 10:49:09 +0600

    Добавьте файлы проекта.

---
+++ Politeh.DAL/ServiceClient.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiteDB;
+using Politeh.DAL.Model;
+
+namespace Politeh.DAL
+{
+    public class ServiceClient
+    {
+        /// <summary>
+        /// Метод который возвращает список всех клиентов
+        /// </summary>
+        /// <returns></returns>
+        public List<Client> GetAllClients()
+        {
+            List<Client> cleints = null;
+
+            using (var db = new LiteDatabase(@"C:\Temp\MyData.db"))
+            {
+                cleints = db.GetCollection<Client>("Client")
+                    .FindAll()
+                    .ToList();
+            }
+
+            return cleints;
+        }
+
+        /// <summary>
+        /// Метод котоырй возвращает клиента по его ID
+        /// </summary>
+        /// <param name="Id">Уникальный номер клиента</param>
+        /// <returns></returns>
+        public Client GetClientById(int Id)
+        {
+            Client client = null;
+            using (var db = new LiteDatabase(@"C:\Temp\MyData.db"))
+            {
+                client = db.GetCollection<Client>("Client")
+                    .FindById(Id);
+            }
+            return client;
+        }
+
+
+        /// <summary>
+        /// Метод который  создает кдлиента
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
+        public bool CreateClient(Client client)
+        {
+            using (var db = new LiteDatabase(@"C:\Temp\MyData.db"))
+            {
+                var clients = db.GetCollection<Client>("Client");
+
+                clients.Insert(client);
+            }
+
+            return true;
+        }
+        //Мет од кооторый обнорвляет клиента
+        //Метод который удаляет клиента
+    }
+}
---
+++ Politeh.Online.View/Program.cs
@@ -0,0 +1,30 @@
+﻿using Politeh.DAL;
+using Politeh.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Politeh.Online.View
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.CreateDate = DateTime.Now;
+            client.Dob = new DateTime(1988, 11, 01);
+            client.Email = "gersen.e.a@gmail.com";
+            
+            client.Password = "123";
+
+            client.FName = "Yevgeniy";
+            client.SName = "Gertsen";
+            client.LName = "";
+
+            ServiceClient service = new ServiceClient();
+            service.CreateClient(client);
+        }
+    }
+}
