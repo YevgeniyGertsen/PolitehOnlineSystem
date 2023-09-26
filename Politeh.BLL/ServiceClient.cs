2521be4ec1de035d1a3f49088dd951ffba144e71
Nurbolat Azhgaliev <nur9609@outlook.com>
2023-09-26 11:01:25 +0600

    Добавили функционал авторизации и регистрации

---
+++ Politeh.BLL/ServiceClient.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Politeh.DAL;
+using Politeh.DAL.Model;
+
+namespace Politeh.BLL
+{
+    public class ServiceClient
+    {
+        private readonly string path = "";
+        public ServiceClient(string path)
+        {
+            this.path = path;
+        }
+
+        public (bool isError, string message) RegisterClient(Client client)
+        {
+            RepositoryClient repo = new RepositoryClient(path);
+
+            ReturnResultClient result = repo.CreateClient(client);
+            
+            //string message = "";
+            //if(result.Exception != null)
+            //{
+            //    message = result.Exception.Message;
+            //}
+            //else
+            //{
+            //    message = "";
+            //}
+
+            return (result.IsError, 
+                result.Exception!=null 
+                 ? result.Exception.Message
+                 : "");
+        }
+
+        public Client AuthorizationClient(Client client)
+        {
+            RepositoryClient repo = new RepositoryClient(path);
+            ReturnResultClient result = repo.GetClient(client.Email, client.Password);
+
+            return result.Client;
+        }
+    }
+}
---
+++ Politeh.DAL/ReturnResultClient.cs
@@ -0,0 +1,18 @@
+﻿using Politeh.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Politeh.DAL
+{
+    public class ReturnResultClient
+    {
+        public bool IsError { get; set; } = false;
+        public string Message { get; set; }
+        public Exception Exception { get; set; }
+        public Client Client { get; set; }
+        public List<Client> Clients { get; set; }
+    }
+}
